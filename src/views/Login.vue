6730b97e489b42536c71a9ab813a47d827755e5c
Karthik Raman <karthikeyan@yuvabe.com>
2022-11-11 08:15:12 +0530

    init

---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <q-input v-model="text" label="Standard" />
+    <q-btn color="primary" icon="check" label="OK" @click="onClick" />
+</template>
+
+<script>
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+
+export default {
+    name: "Login",
+    setup() {
+        const router = useRouter();
+        const text = ref("")
+
+        function onClick() {
+            router.push("/")
+        }
+
+        return {
+            onClick,
+            text
+        }
+    },
+}
+</script>
