801b7b5b42068197c047d2f72a81852f46422ed4
Karthik Raman <karthikeyan@yuvabe.com>
2022-11-11 08:15:17 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header v-show="is_logged_in" elevated class="text-white bg-dark">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          Future School
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-show="is_logged_in" show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: "App",
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const is_logged_in = ref(false)
+
+    return {
+      leftDrawerOpen,
+      is_logged_in,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
